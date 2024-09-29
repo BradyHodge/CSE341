b0bd0468f8965d3234b5db1d0ec7366e119efb52
Brady <bradyhodge1@gmail.com>
2024-09-28 22:43:05 -0600

    done

--- L01Team/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const getHelloWorld = (req, res) => {
-    res.json('Hello World!');
-};
-
-const getPerson = (req, res) => {
-    res.json('Bailee Hodge');
-}
-
-module.exports = { getHelloWorld , getPerson };
--- L01Team/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use('/', require('./routes/index'));
-
-app.listen(3000, function() {
-    console.log('Server running on port 3000');
-    }
-);  
