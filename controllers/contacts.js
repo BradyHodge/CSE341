ec636d59e068121919d40788b7f7e05079afb281
Brady <bradyhodge1@gmail.com>
2024-10-05 11:37:24 -0600

    done

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -19,11 +19,45 @@ const getSingle = async (req, res, next) => {
     res.status(200).json(lists[0]);
   });
 };
+
 const createContact = async (contact) => {
-  const db = await getDB();
-  const result = await db.collection('contacts').insertOne(contact);
-  return result.insertedId;
+  try {
+    const db = await getDB();
+    const result = await db.collection('contacts').insertOne(contact);
+    return result.insertedId;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
 
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const db = await getDB();
+  const updatedData = req.body;
+  delete updatedData._id;
+  const result = await db.collection('contacts').updateOne({ _id: userId }, { $set: updatedData });
+  if (result.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(404).json({ error: 'Contact not found' });
+  }
+};
 
-module.exports = { getAll, getSingle, createContact };
+const deleteContact = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const db = await getDB();
+    const result = await db.collection('contacts').deleteOne({ _id: userId });
+    if (result.deletedCount > 0) {
+      res.status(200).json({});
+    } else {
+      res.status(404).json({ error: 'Contact not found' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,21 +1,24 @@
 const express = require('express');
-const { getAll, getSingle, createContact } = require('../controllers/contacts');
+const { getAll, getSingle, createContact, updateContact, deleteContact } = require('../controllers/contacts');
+
 const router = express.Router();
 
-
 router.get('/', getAll);
+
 router.get('/:id', getSingle);
+
 router.post('/', async (req, res) => {
     try {
         const newContact = await createContact(req.body);
-        console.log(req.body);
-        
-
-        return res.status(201).json({ id: newContact._id });
+        return res.status(201).json({ id: newContact });
     } catch (err) {
         console.error(err);
-
         res.sendStatus(500);
     }
 });
+
+router.put('/:id', updateContact);
+
+router.delete('/:id', deleteContact);
+
 module.exports = router;
