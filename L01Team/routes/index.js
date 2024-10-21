08eb1ffb1e3c1a92bdb353de9f6f57857696b065
BradyHodge <bradyhodge1@gmail.com>
2024-10-20 21:55:04 -0600

    publish backend for render

--- L01Team/routes/index.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-
-const myController = require('../controllers/index');
-
-routes.get('/', myController.getPerson);
-routes.get('/hello', myController.getHelloWorld);
-
-module.exports = routes;
--- L02Personal/backend/routes/professional.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const proController = require('../controllers/professional');
-
-const router = express.Router();
-
-router.get('/', proController.getData);
-module.exports = router;
