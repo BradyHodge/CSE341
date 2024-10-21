4333bcc3eddee4cb725a8a2f09007187b22a9d23
BradyHodge <bradyhodge1@gmail.com>
2024-10-20 21:56:53 -0600

    update swagger to use render

--- swagger.js
+++ swagger.js
@@ -4,8 +4,8 @@ const doc = {
     title: 'My API',
     description: 'Users API',
   },
-  host: 'localhost:8080',
-  schemes: ['http'],
+  host: 'cse341-1s59.onrender.com',
+  schemes: ['https'],
 };
 const outputFile = './swagger.json';
 const endpointsFiles = ['./app.js'];
